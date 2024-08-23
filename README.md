b0428c87c8aaa0ac5734417d4235d569011c5150
KarlMarxArchX <KarlMarxArchX97@Outlook.Com>
2024-08-24 02:08:20 +0530

    Initial: Updated To Arch-Linux

---
+++ README.md
@@ -0,0 +1,58 @@
+# Termux Setup
+
+### A script to auto-install few packages and customise termux.
+![image](https://github.com/anonymousx97/termux-setup/assets/88324835/424eafa8-d0bb-4b6b-a033-b45b5d878dcc)
+
+
+>It was made because Ryuk was ~~LAZY~~ tired of setting up termux again and again. XD
+
+### Previews
+<details>
+<summary>  termux-url-opener:</summary>
+
+https://github.com/anonymousx97/termux-setup/assets/88324835/9ba12b6a-e53c-47df-88b8-51585666cd5e
+
+</details>
+<details>
+<summary>  arc:</summary>
+
+https://github.com/anonymousx97/termux-setup/assets/88324835/a1067ff9-9c31-40d8-82bf-c2c10dbc11f3
+
+</details>
+
+### Installation
+Download Termux:
+[**GitHub**](https://github.com/termux/termux-app/releases/download/v0.118.0/termux-app_v0.118.0+github-debug_universal.apk) | [**F-Droid**](https://f-droid.org/repo/com.termux_118.apk)  
+
+Run:  
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/anonymousx97/termux-setup/main/setup.sh)"
+  ```
+### Credits 
+* By: Ryuk [ [TG](https://t.me/anonymousx97) | [GitHub](https://github.com/anonymousx97) ]
+* [mayTrmux](https://github.com/mayTermux) for original _color.properties & rxfetch_
+* Packages Used:
+  * APT :
+
+        aria2 
+        curl 
+        ffmpeg 
+        git 
+        gh 
+        openssh 
+        python 
+        python-pip 
+        python-pillow 
+        sshpass 
+        tmux 
+        tsu 
+        wget 
+
+  * PIP :
+
+        yt-dlp
+        black
+        isort
+        autopep8
+        autoflake
---
+++ setup.sh
@@ -0,0 +1,324 @@
+#!/bin/bash
+
+# By Ryuk Github | TG : [ anonymousx97 ]
+# Fork| Github | TG : | KarlMarxArchX97 |
+
+red="\033[1;31m"
+white="\033[0m"
+green="\033[1;32m"
+defaults=false
+
+start(){
+    clear
+    local menu="${green}Main Menu:${white}"
+    local options="
+  1. Install Essential packages.
+  2. Customize Termux.
+  3. Both 1 & 2 (Uses Presets for customisation)
+  4. Setup Arch-Linux in Termux.
+  5. Exit
+"
+    all(){
+        defaults=true
+        package_setup
+        customize
+    }
+    declare -A start_options
+    start_options[1]="package_setup"
+    start_options[2]="customize"
+    start_options[3]="all"
+    start_options[4]="setup_archlinux"
+    start_options[5]="exit_"
+    ask "${options}" "${menu}" "start_options"
+    if ! [ "$( ls /sdcard/ 2> /dev/null )" ]; then
+        termux-setup-storage
+    fi
+}
+
+
+
+customize(){
+    clear
+    local menu="${green}Customisation Menu:${white}"
+    customize_all(){
+        echo -e "${green}Customising All.${white}"
+        defaults=true
+        setup_aria2
+        setup_ytdlp
+        setup_prettify
+        setup_rxfetch
+        change_cursor
+        change_ui
+        sleep 2
+        clear
+        rxfetch
+    }
+    if $defaults ; then
+        customize_all
+    else
+        declare -A option_dict
+        option_dict[1]="setup_apm"
+        option_dict[2]="setup_aria2"
+        option_dict[3]="setup_ytdlp"
+        option_dict[4]="setup_prettify"
+        option_dict[5]="setup_rxfetch"
+        option_dict[6]="change_cursor"
+        option_dict[7]="change_ui"
+        option_dict[8]="customize_all"
+        option_dict[9]="start"
+        option_dict[10]="exit_"
+        local options="
+  1. Setup Android Package Manager by Ryuk.
+  2. Setup Aria2 Shortcut.
+  3. Enable Downloading Audio or Video
+     by sharing YT-DLP supported links to termux.
+  4. Setup 'prettify' Bunch of py formatting tools.
+  5. Setup Rxfetch.
+  6. Change Cursor Style.
+  7. Change Colour and Font.
+  8. All of the above. ( Uses Presets )
+  9. Go back to previous menu.
+  10. Exit
+"
+        ask "${options}" "${menu}" "option_dict"
+    fi
+}
+
+
+ask(){
+    local header="What do you want to do?"
+    local option_text="$1"
+    local menu="$2"
+    local dict_name=$3
+    echo -e "${menu}\n${header}${option_text}"
+    while true;do
+        read -p "> " choice
+        if [ -z $choice ]; then
+            clear
+            echo -e "${menu}\n${header}${red}\n Choose an Option.${white}${option_text}"
+        elif [[ -v ${dict_name}[$choice] ]]; then
+            eval "\${${dict_name}[$choice]}"
+            break 
+        else
+            clear
+            echo -e "${menu}\n${header}${red}\n Invalid Input: ${white}${choice}${option_text}"
+        fi
+    done
+}
+
+
+package_setup(){
+    # Update Termux Package Repository
+    termux-change-repo
+    yes|pkg upgrade
+    apt update -y 
+    apt upgrade -y
+
+    # Install necessary packages
+    apt install -y \
+        aria2 \
+        curl \
+        ffmpeg \
+        git \
+        gh \
+        openssh \
+        python \
+        python-pip \
+        python-pillow \
+        sshpass \
+        tmux \
+        tsu \
+        wget \
+
+    # Update and Install pip packages
+    pip install -U \
+        wheel \
+        setuptools \
+        yt-dlp \
+        black \
+        isort \
+        autopep8 \
+        autoflake
+}
+
+
+setup_archlinux(){
+    apt update
+    apt install -y root-repo x11-repo
+    apt install -y \
+    proot \
+    proot-distro \
+    termux-x11-nightly \
+    pulseaudio 
+
+    proot-distro install archlinux
+    clear
+    local options="
+1. Install XFCE4
+2. Install KDE-Plasma
+3. Exit
+"
+    declare -A wm_dict
+    wm_dict[1]="export wm=xfce4 wm_cmd=startxfce4"
+    wm_dict[2]="export wm=kde-standard wm_cmd=startplasma-x11"
+    wm_dict[3]="exit_"
+
+    ask "${options}" "Window Manager Menu:" "wm_dict"
+
+    proot-distro login archlinux  --termux-home --shared-tmp -- bash -c "
+    pacman -Syu  && \
+    pacman -S \
+    firefox-esr \
+    bpytop \
+    fastfetch \
+    openssh \
+    ${wm} \
+    xorg-server \ 
+    xorg-xinit \
+    xorg-utils \
+    xterm xfce4 \
+    xfce4-goodies \
+    locales \
+    fonts-noto-cjk && \
+    ln -sf /usr/share/zoneinfo/Asia/Calcutta /etc/localtime && \
+    echo en_US.UTF-8 UTF-8 >> /etc/locale.gen && \
+    locale-gen && \
+    echo 'LANG=en_US.UTF-8' > /etc/locale.conf "
+
+    curl -s -O --output-dir $HOME https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/scripts/archlinux.sh
+    sed -i "s/wm_start_cmd/${wm_cmd}/" $HOME/archlinux.sh
+    echo -e '\nalias acli="proot-distro login archlinux --termux-home --shared-tmp -- bash" \nalias dgui="bash debian.sh"\n' >> $HOME/.bashrc
+    echo '
+check_distro(){
+    if [[ "$(whoami)" == "root" ]]; then
+        export HISTFILE=~/.archlinux_history
+    fi
+}
+check_distro
+' >> $HOME/.bashrc
+    echo "Done."
+    echo -e "You can now use '${green}dcli${white}' for archlinux cli and '${green}dgui${white}' for GUI (Termux x11 app required)."
+}
+
+
+setup_apm(){
+    echo -e "\n1. Downloading Android Package Manager By Ryuk."
+    curl -s -O --output-dir $PATH https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/bin/apm
+    chmod u+x $PATH/apm
+    echo -e "${green}Done.${white} use 'apm' to call it."
+}
+
+
+setup_aria2(){
+    echo -e "\n2. Downloading Aria2 shortcut"
+    if ! [ -f $PATH/arc ] || $defaults ; then
+        curl -s -O --output-dir $PATH https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/bin/arc
+        chmod u+x $PATH/arc
+        echo -e "${green}Done.${white}"
+    else
+        echo -e "${red}A script with the name 'arc' exists in ${PATH}${white}"
+    fi
+}
+
+
+setup_ytdlp(){
+    echo -e "\n3. Downloading files and Setting Up YT-DLP link share Trigger."
+    mkdir -p $HOME/bin
+    curl -s -O --output-dir $HOME/bin https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/bin/termux-url-opener
+    echo -e "${green}Done.${white}"
+}
+
+setup_prettify(){
+    echo -e "\n4. Downloading and Setting up Prettify script." 
+    curl -s -O --output-dir $PATH https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/bin/prettify
+    chmod u+x $PATH/prettify
+    echo -e "${green}Done.${white}"
+}
+
+setup_rxfetch(){
+    echo -e "\n5. Downloading and Setting up Rxfetch"
+    curl -s -O --output-dir $PATH https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/bin/rxfetch
+    chmod u+x $PATH/rxfetch
+    local motd="#!$SHELL\nbash rxfetch"
+
+    if [ -f ~/.termux/motd.sh ] && ! $defaults ; then
+        echo -e "${red}A custom start script exists in the path ${HOME}/.termux/motd.sh${white}"
+        echo -e "  Enter 1 to overwrite the current file.\n  Press Enter to skip."
+        read -p "> " prompt_
+        if ! [ $prompt_ ] ||  ! [ $prompt_ == 1 ] ; then
+            echo -e "${green}Skipping motd modification.${white}"
+        else
+            echo -e "${red}Overwriting motd.${white}"
+            echo -e $motd > ~/.termux/motd.sh
+        fi
+    else
+        echo -e $motd > ~/.termux/motd.sh
+    fi
+    echo -e "${green}Done.${white}"
+}
+
+
+change_cursor(){
+    echo -e "\n6. Changing Cursor"
+    local menu="Cursor Menu:"
+    local options="
+  1. Change to ${green}|${white} (bar)
+  2. Change to ${green}_${white} (underscore)
+  3. Change to Default Block style.
+  4. Exit
+"
+    if ! $defaults ; then
+        clear
+        declare -A cursor_dict
+        cursor_dict[1]="eval printf '\e[6 q' && export style=bar"
+        cursor_dict[2]="eval printf '\e[4 q' && export style=underline"
+        cursor_dict[3]="eval printf '\e[1 q' && export style=block"
+        cursor_dict[4]="exit_"
+        ask "${options}" "${menu}" "cursor_dict"
+    else
+        printf '\e[6 q'
+        style=bar
+    fi
+    # Set the style in termux properties 
+    sed -i "s/.*terminal-cursor-style.*/terminal-cursor-style = ${style}/" $HOME/.termux/termux.properties
+    # Change Blink Rate
+    sed -i "s/.*terminal-cursor-blink-rate.*/terminal-cursor-blink-rate = 600/" $HOME/.termux/termux.properties
+   echo -e "${green}Done.${white}"
+}
+
+
+change_ui(){
+    local colors="colors.properties.dark_blue"
+    if ! $defaults; then 
+        echo -e "\n7. Changing Colour and Font."
+        local ui_options="\n1. Set Dark Blue\n2. Set Light Blue"
+        declare -A ui_dict
+        ui_dict[1]="export colors=colors.properties.dark_blue"
+        ui_dict[2]="export colors=colors.properties.light_blue"
+        clear
+        ask "${ui_options}" "${green}UI Menu${white}" "ui_dict"
+    fi
+    curl -s -o $HOME/.termux/colors.properties https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/.termux/"${colors}"
+    wget -q -O $HOME/.termux/font.ttf https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/.termux/MesloLGS_NF_Bold.ttf
+    echo -e "\n${green}Applying Changes.${white}"
+    termux-reload-settings
+    echo -e "${green}Done.${white}"
+}
+
+
+exit_(){
+    echo -e "${green}Exiting...${white}"
+    exit
+}
+
+save_setup_sh(){
+    if ! [ -f $PATH/setup-termux ]; then
+        echo -e "\nSaving setup.sh for future use."
+        echo -e '#!/bin/bash\nbash -c "$(curl -fsSL https://raw.githubusercontent.com/KarlMarxArchX/termux-setup/main/setup.sh)"' > $PATH/setup-termux
+        chmod u+x $PATH/setup-termux
+        echo -e "${green}Done\n${white}You can now use ${green}'setup-termux'${white} to get back to this menu."
+    fi
+}
+
+start
+save_setup_sh
